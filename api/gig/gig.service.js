ccdb67e8186480d47a9752a762536a4f0978761a
Shlomi Bazar <shlomibazz@gmail.com>
2022-12-06 11:31:02 +0200

    first commit

---
+++ api/gig/gig.service.js
@@ -0,0 +1,123 @@
+const dbService = require('../../services/db.service')
+const logger = require('../../services/logger.service')
+const utilService = require('../../services/util.service')
+const ObjectId = require('mongodb').ObjectId
+
+
+
+async function query(filterBy={txt:''}) {
+    try {
+        const criteria = {
+            title: { $regex: filterBy.txt, $options: 'i' }
+        }
+        const collection = await dbService.getCollection('gig')
+        var gigs = await collection.find(criteria).toArray()
+        // const gigs = collection.find({ buyerId: id }).toArray()
+
+        return gigs
+    } catch (err) {
+        logger.error('cannot find gigs', err)
+        throw err
+    }
+}
+
+async function getById(gigId) {
+    try {
+        const collection = await dbService.getCollection('gig')
+        const gig = collection.findOne({ _id: ObjectId(gigId) })
+        return gig
+    } catch (err) {
+        logger.error(`while finding gig ${gigId}`, err)
+        throw err
+    }
+}
+
+async function remove(gigId) {
+    try {
+        const collection = await dbService.getCollection('gig')
+        await collection.deleteOne({ _id: ObjectId(gigId) })
+        return gigId
+    } catch (err) {
+        logger.error(`cannot remove gig ${gigId}`, err)
+        throw err
+    }
+}
+
+async function add(gig) {
+    try {
+        const collection = await dbService.getCollection('gig')
+        await collection.insertOne(gig)
+        return gig
+    } catch (err) {
+        logger.error('cannot insert gig', err)
+        throw err
+    }
+}
+
+async function update(gig) {
+    try {
+        const gigToSave = {
+            vendor: gig.vendor,
+            price: gig.price
+        }
+        const collection = await dbService.getCollection('gig')
+        await collection.updateOne({ _id: ObjectId(gig._id) }, { $set: gigToSave })
+        return gig
+    } catch (err) {
+        logger.error(`cannot update gig ${gigId}`, err)
+        throw err
+    }
+}
+
+async function addGigMsg(gigId, msg) {
+    try {
+        msg.id = utilService.makeId()
+        const collection = await dbService.getCollection('gig')
+        await collection.updateOne({ _id: ObjectId(gigId) }, { $push: { msgs: msg } })
+        return msg
+    } catch (err) {
+        logger.error(`cannot add gig msg ${gigId}`, err)
+        throw err
+    }
+}
+
+async function removeGigMsg(gigId, msgId) {
+    try {
+        const collection = await dbService.getCollection('gig')
+        await collection.updateOne({ _id: ObjectId(gigId) }, { $pull: { msgs: {id: msgId} } })
+        return msgId
+    } catch (err) {
+        logger.error(`cannot add gig msg ${gigId}`, err)
+        throw err
+    }
+}
+function _buildCriteria(filterBy) {
+    const criteria = {}
+  
+    // by name
+    // const regex = new RegExp(filterBy.name, 'i')
+    // criteria.name = { $regex: regex }
+  
+    // filter by inStock
+    // if (filterBy.inStock) {
+    //   criteria.inStock = { $eq: JSON.parse(filterBy.inStock) }
+    // }
+  
+    // filter by labels
+    // if (filterBy.labels?.length) {
+    //   criteria.labels = { $in: filterBy.labels }
+    // }
+    // return criteria
+  
+  }
+  
+
+module.exports = {
+    remove,
+    query,
+    getById,
+    add,
+    update,
+    addGigMsg,
+    removeGigMsg,
+}
